1d30c3dfe6b4e64433b1fc4d410de9f2e19a32b5
Ikram_Lim <ikramlim@gmail.com>
2015-12-18 17:11:48 +0800

    for loop
    for loop

---
+++ forloop/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for(i=1; i<=5; i++)
+            {
+                for(j=1; j<= i; j++)
+                {
+                    Console.Write(j); //or can replace this to ("*")
+                }
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+/*
+    1
+    12
+    123
+    1234
+    12345
+
+
+    or
+
+    *
+    **
+    ***
+    ****
+    *****
+*/
